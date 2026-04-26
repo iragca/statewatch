d1e83cdfac9bcaeacd4d366b440a986f6ec57872
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-04-26 17:12:45 +0800

    🔧 update: implement Service class for session management with context management support

---
+++ statewatch/services/service.py
@@ -0,0 +1,13 @@
+class Service:
+    def __init__(self, session):
+        self.session = session
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc, tb):
+        if exc_type:
+            self.session.rollback()
+        else:
+            self.session.commit()
+        self.session.close()
