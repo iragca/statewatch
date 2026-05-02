ef6a0b516227b7b28b29c92762f0358e6510f1cd
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-05-02 22:21:57 +0800

    🔧 update: add update_all_prices endpoint to update prices of all assets in the database

--- statewatch/routes/tasks.py
+++ statewatch/routes/tasks.py
@@ -5,11 +5,15 @@ from fastapi import APIRouter
 
 from statewatch.core.config import env
 from statewatch.dependencies.auth import AuthenticatedUser
-from statewatch.dependencies.services import Asset_Service, Price_Service
 from statewatch.dependencies.db import DB_Session
+from statewatch.dependencies.services import Asset_Service, Price_Service
 from statewatch.schemas.enums import AssetClass
-from statewatch.scrapers import ALPHAVANTAGEScraper
-from statewatch.services import TransactionOrchestrator, AssetService, PriceService
+from statewatch.scrapers import (
+    ALPHAVANTAGEScraper,
+    CryptocurrencyScraper,
+    YFinanceScraper,
+)
+from statewatch.services import AssetService, PriceService, TransactionOrchestrator
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
@@ -69,4 +73,44 @@ async def add_price(
 
     return {"message": f"Asset {ticker} and its price history added successfully"}
 
-    
+
+@router.get("/update_all")
+async def update_all_prices(
+    DB_session: DB_Session,
+    _: AuthenticatedUser,
+):
+    """
+    Update the prices of all cryptocurrencies in the database.
+
+    Access Level: Authenticated users only.
+    """
+    with TransactionOrchestrator(DB_session) as transaction:
+        asset_service = AssetService(transaction.session)
+        price_service = PriceService(transaction.session)
+
+        assets = asset_service.get_all_assets()
+
+        for asset in assets:
+            if asset.asset_class == AssetClass.CRYPTOCURRENCY:
+                scraper = CryptocurrencyScraper(env.COINGECKO_DEMO_API_KEY)
+
+                price = await scraper.get_price_by_date(
+                    name=asset.name.lower(), date=datetime.now(tz=pytz.timezone(env.TIMEZONE))
+                )
+
+                price_service.add_price(
+                    price=float(price),
+                    date=datetime.now(tz=pytz.timezone(env.TIMEZONE)).date(),
+                    asset_id=asset.id,
+                )
+            else:
+                scraper = YFinanceScraper()
+                delay = timedelta(days=2)
+                date = datetime.now(tz=pytz.timezone(env.TIMEZONE)).date()
+                delayed_date = datetime.now(tz=pytz.timezone(env.TIMEZONE)).date() - delay
+                price = scraper.get_price_by_date(ticker=asset.ticker, date=delayed_date)
+                price_service.add_price(
+                    price=float(price),
+                    date=date,
+                    asset_id=asset.id,
+                )
