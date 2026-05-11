c3f5ce190bc42bc63a0b54e01b42850adb770e2d
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-05-11 09:10:44 +0800

    🔧 update: enhance price history endpoint to support CSV response

--- statewatch/routes/price.py
+++ statewatch/routes/price.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter, status
-from statewatch.dependencies.services import Price_Service
-from statewatch.schemas import price as price_schema
 from typing import List
+
+from fastapi import APIRouter, status
+from fastapi.responses import Response
+
+from statewatch.dependencies.services import Price_Service
+from statewatch.formats import CSV
+from statewatch.schemas import price as price_schema
 
 router = APIRouter(prefix="/price", tags=["Price"])
 
@@ -19,14 +23,31 @@ async def read_price_latest(
     return price_schema.Root.model_validate(price)
 
 
-@router.get("/history/{ticker}", status_code=status.HTTP_200_OK)
+@router.get(
+    "/history/{ticker}",
+    status_code=status.HTTP_200_OK,
+    response_model=List[price_schema.Root],
+    responses={
+        200: {"content:": {"text/csv": {}}},
+    },
+)
 async def read_price_history(
-    ticker: str, price_service: Price_Service
-) -> List[price_schema.Root]:
+    ticker: str, price_service: Price_Service, csv: bool = False
+):
     """
     Get the price history of a stock by its ticker symbol.
 
     Access Level: Public
     """
     prices = price_service.get_price_history(ticker)
+
+    if csv:
+        return Response(
+            content=str(CSV.from_price_list(prices)),
+            media_type="text/csv",
+            headers={
+                "Content-Disposition": f"attachment; filename={ticker}_price_history.csv"
+            },
+        )
+
     return [price_schema.Root.model_validate(price) for price in prices]
